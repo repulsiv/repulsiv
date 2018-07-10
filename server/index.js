cd4a10667567af44cd7495c755a4b9f54e0f36bf
key-er <email@example.com>
2018-07-09 23:25:23 -0700

    changed port

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.get('/users', function (req, res) {
   })
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
-});
 
+const port = process.env.PORT || 3000
+app.listen(port, function() {
+  console.log(`listening on port ${port}!`);
+});
