d1393866de6f3d13cec67cc7f31f676cc077ceb6
key-er <email@example.com>
2018-07-10 17:10:00 -0700

    try catch for defining schema

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -10,17 +10,24 @@ catch(e) {
     HOST    : process.env.CLEARDB_DATABASE_HOST,
     USER    : process.env.CLEARDB_DATABASE_USER,
     PASSWD  : process.env.CLEARDB_DATABASE_PASSWD,
-    DATABASE : process.env.CLEARDB_DATABASE
+    DATABASE : process.env.CLEARDB_DATABASE,
+    PORT: 3306
   }
 }
 
 // to connect  mysql -u <username> -p -h us-cdbr-iron-east-04.cleardb.net
 
+try {
+  var sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWD, {
+    host: 'localhost',
+    dialect: 'mysql',
+  });
+}
 
-const sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWD, {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+
+catch(e) {
+  console.log(e)
+}
 
 const User = sequelize.define('user', {
   username: Sequelize.STRING,
