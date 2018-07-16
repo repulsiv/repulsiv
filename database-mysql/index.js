b730a684b6ac326ae414bbb25c31c3325ef1bea0
key-er <email@example.com>
2018-07-15 18:05:35 -0700

    added productDetail Router after fetching auth changes

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,4 +1,40 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+// try {
+
+//   config = require('./config.js')
+// }
+
+
+// catch(e) {
+//   config = {
+//     HOST    : process.env.CLEARDB_DATABASE_HOST,
+//     USER    : process.env.CLEARDB_DATABASE_USER,
+//     PASSWD  : process.env.CLEARDB_DATABASE_PASSWD,
+//     DATABASE : process.env.CLEARDB_DATABASE,
+//     CLIENT_ID: process.env.CLIENT_ID,
+//     PORT: 3306
+//   }
+// }
+
+// // to connect  mysql -u <username> -p -h us-cdbr-iron-east-04.cleardb.net
+
+// try {
+//   var sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWD, {
+//   host: config.HOST,
+//   dialect: 'mysql',
+//   });
+
+// }
+
+
+// catch(e) {
+//   console.log(e)
+// }
+
+
+////// JUST FOR TEST ---- for authentication part
+
+var mysql = require('mysql');
 try {
 
   config = require('./config.js')
@@ -15,70 +51,49 @@ catch(e) {
     PORT: 3306
   }
 }
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : config.USER,
+  password : config.PASSWD,
+  database : config.DATABASE
+});
 
-// to connect  mysql -u <username> -p -h us-cdbr-iron-east-04.cleardb.net
-
-try {
-  var sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWD, {
-  host: config.HOST,
-  dialect: 'mysql',
+var selectAll = function(callback) {
+  connection.query('SELECT * FROM users', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
   });
+};
 
+
+var findUserId = function(userId, callback) {
+
+  connection.query('SELECT * FROM users WHERE userId = ?', userId, function(err, results, fields) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results)
+    }
+  })
 }
 
+var insertUserId = function(userInfo, callback) {
+  // userInfo = {sub: 1221233223, email:'abc@yahoo.com', username:'userABC'}
 
-catch(e) {
-  console.log(e)
+  connection.query('INSERT INTO users SET ?', userInfo, function(err, results, fields) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results)
+    }
+  })
 }
 
-
-// JUST FOR TEST ---- for authentication part
-
-// var mysql = require('mysql');
-
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : <username>,
-//   password : <passoword>,
-//   database : 'greenfield'
-// });
-
-// var selectAll = function(callback) {
-//   connection.query('SELECT * FROM users', function(err, results, fields) {
-//     if(err) {
-//       callback(err, null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
-
-
-// var findUserId = function(userId, callback) {
-
-//   connection.query('SELECT * FROM users WHERE userId = ?', userId, function(err, results, fields) {
-//     if (err) {
-//       callback(err, null);
-//     } else {
-//       callback(null, results)
-//     }
-//   })
-// }
-
-// var insertUserId = function(userInfo, callback) {
-//   // userInfo = {sub: 1221233223, email:'abc@yahoo.com', username:'userABC'}
-
-//   connection.query('INSERT INTO users SET ?', userInfo, function(err, results, fields) {
-//     if (err) {
-//       callback(err, null);
-//     } else {
-//       callback(null, results)
-//     }
-//   })
-// }
-
-// module.exports = {
-//   selectAll: selectAll,
-//   findUserId: findUserId,
-//   insertUserId: insertUserId
-// }
+module.exports = {
+  selectAll: selectAll,
+  findUserId: findUserId,
+  insertUserId: insertUserId
+}
--- react-client/src/components/ProductListItem.jsx
+++ react-client/src/components/ProductListItem.jsx
@@ -1,3 +1,38 @@
+import React from 'react';
+import Toggle from 'react-toggle'
+import ProductDetail from './ProductDetail.jsx'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import {Route, NavLink,HashRouter} from "react-router-dom";
+
+
+const ProductListItem = (props) => {
+
+  let toggleSwitch;
+  if (props.isLoggedIn) {
+    toggleSwitch = <span> <Toggle /> </span>
+  }
+
+  return (
+    <Router>
+      <div>
+        <li>
+          <Link to=
+          {{ pathname: '/products', search: `?id=${props.value.itemId}` }} > { props.value.name } Price:${props.value.msrp}
+          {toggleSwitch}
+          </Link>
+        </li>
+      </div>
+    </Router>
+    )
+}
+
+
+export default ProductListItem;
+
+
+
 // import React from 'react';
 // import Toggle from 'react-toggle'
 // import ProductDetail from './ProductDetail.jsx'
